62ab5a794040f77c10676e5a4eb3b8d39676bf10
Liman <saniahmadliman@gmail.com>
2024-08-26 12:00:48 -0800

    added some pages

--- lib/main.dart
+++ lib/main.dart
@@ -27,12 +27,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
